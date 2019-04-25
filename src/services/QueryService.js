84bcd9b83ef47a8d3ab781e8449d1170a6be9543
Julius ANGELES <julius.angeles.ext@louisvuitton.com>
2019-04-25 17:42:09 +0800

    Added query for elasticsearch

--- src/services/QueryService.js
+++ src/services/QueryService.js
@@ -1,6 +1,79 @@
 // TODO: Write the functions for the ElasticSearch queries in this module
 // These are to be injected into the relevant components, e.g. APIStatusDasboard
 
-export default function() {
-  return "TODO: Fetch API statuses from ElasticSearch here";
+
+export default function(servicesEnum) {
+  let apiSize = Object.keys(servicesEnum).length;
+  let i = 0;
+  let string = "";
+
+  Object.keys(servicesEnum).forEach(function(api) {
+    if(i < (apiSize-1)) {
+      let stringBuilder =
+        `"${api}" : {
+          "filter" : {
+            "bool" : {
+              "must": [
+                {
+                  "match": {
+                    "status": "ERROR"
+                  }
+                },
+                {
+                  "range": {
+                    "creationDate": {
+                      "gte": "${servicesEnum[api].from}",
+                      "lte": "${servicesEnum[api].to}"
+                    }
+                  }
+                },
+                {
+                  "match": {
+                    "apiName": "${servicesEnum[api].apiName}"
+                  }
+                }
+              ]
+            }
+          }
+      },`;
+      string += stringBuilder;
+    }else {
+      let stringBuilder =
+        `"${api}" : {
+          "filter" : {
+            "bool" : {
+              "must": [
+                {
+                  "match": {
+                    "status": "ERROR"
+                  }
+                },
+                {
+                  "range": {
+                    "creationDate": {
+                      "gte": "${servicesEnum[api].from}",
+                      "lte": "${servicesEnum[api].to}"
+                    }
+                  }
+                },
+                {
+                  "match": {
+                    "apiName": "${servicesEnum[api].apiName}"
+                  }
+                }
+              ]
+            }
+          }
+      }`;
+      string += stringBuilder;
+    }
+    i++;
+  });
+  let apiQuery =`{
+    "size" : 0,
+    "aggs" : {
+      ${string}
+    }
+  }`;
+  return apiQuery;
 }
