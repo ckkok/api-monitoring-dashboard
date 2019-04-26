abb1ae3cbaaf668e6beb08ccf848037c3e2c1452
CK <kok.chee.kean@outlook.com>
2019-04-26 16:46:55 +0800

    Configured APIs in config.json

--- src/services/QueryService.js
+++ src/services/QueryService.js
@@ -1,6 +1,7 @@
 // TODO: Write the functions for the ElasticSearch queries in this module
 // These are to be injected into the relevant components, e.g. APIStatusDasboard
+import fs from 'fs';
 
 export default function() {
-  return "TODO: Fetch API statuses from ElasticSearch here";
+  return fs.readFileSync('@/mockResponse.json', 'utf8');
 }
